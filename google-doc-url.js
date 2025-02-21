653f4e01dcb5b906ec2eb74aa238a0ce175a37e5
secondanatura <fabio.medir@gmail.com>
2025-02-21 14:41:46 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://secondanatura.github.io/leaflet_camogli/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yOMmAgfUCC7CrjFtbqBhDFS3mHRsv8HnmvGULvzW7HY/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
